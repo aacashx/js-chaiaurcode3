afa61e431cd8a68b4cbd5169af60d3a78b45937f
Aakash <aakashkumarx13@gmail.com>
2025-12-06 21:20:01 +0000

    datatype summary

---
+++ 01_basic/05_datatypeSummary.js
@@ -0,0 +1,57 @@
+// Primitive data type
+// 7 types : String, Numbers, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.24
+
+const isLoggedIn = false
+const outSideTemperature = null
+let userName;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+// const bigNumber = BigInt(2**3)
+// console.log(bigNumber);
+
+const bigNumber2 = 23434234234n
+
+
+
+
+// Refernce Type or Non-primitive 
+// Object, Array, Functions
+
+const aarryHeros = ["Superman", "Spiderman", "Krish", "Shaktiman"];
+
+let myObj = {
+    name: "Aakash",
+    age: 23, 
+}
+
+const myFunction = function(){
+    console.log("Hello Tivsha")
+}
+
+
+// console.log(typeof bigNumber);
+console.log(typeof bigNumber2);
+
+console.log(typeof myFunction);
+console.log(typeof id);
+
+
+
+
+
+
+
+
+
+
+
+
+
