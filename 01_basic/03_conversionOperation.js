8b855b2b69653b8c32f5ad0db66463b7926e5a3c
Aakash <aakashkumarx13@gmail.com>
2025-12-06 20:37:52 +0000

    Comparison

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,37 +1,87 @@
-let score = "300abc"
+//----CONVERSIOIN----
+// let score = "300abc"
 
-// console.log(typeof score);
-// console.log(typeof (score));
+// // console.log(typeof score);
+// // console.log(typeof (score));
 
-let valueInNumber = Number(score);
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
-
-
-//"300" => 300
-//"300abc" => NaN
-//true => 1
-//false => 0
-//null => 0
-//undefined => NaN
+// let valueInNumber = Number(score);
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber);
 
 
-let isLoggedIn = 1;
-
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-// console.log(booleanIsLoggedIn);
-
-//1 => true
-//0 => false
-//"Aakash" => true
-//"" => false
-//null => false
-//undefined => false
-//NaN => false  
+// //"300" => 300
+// //"300abc" => NaN
+// //true => 1
+// //false => 0
+// //null => 0
+// //undefined => NaN
 
 
-let someNumber = 123;
+// let isLoggedIn = 1;
 
-let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// // console.log(booleanIsLoggedIn);
+
+// //1 => true
+// //0 => false
+// //"Aakash" => true
+// //"" => false
+// //null => false
+// //undefined => false
+// //NaN => false  
+
+
+// let someNumber = 123;
+
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+//----OPERATIONS----
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str = "hello"
+// let str2 = " Tvisha"
+
+// let str3 = str + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+// console.log(-false);
+
+
+let gameCounter = 100
+gameCounter--
+gameCounter--
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+ 
+
+
---
+++ 01_basic/04_comparisons.js
@@ -0,0 +1,24 @@
+// console.log(2>3);
+// console.log(2<4);
+// console.log(2==4);
+// console.log(2<=4);
+// console.log(2>=4);
+// console.log(2!=4);
+
+// console.log("3">3);
+// console.log("03"<=3);
+ 
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null < 0);
+// console.log(null > 0);
+// console.log(null != 0);
+
+
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+console.log(undefined == 0);
+console.log(undefined != 0);
+
+
