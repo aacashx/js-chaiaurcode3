0f4491c3e767d2186e8337e3b043798ff109a563
Aakash <aakashkumarx13@gmail.com>
2025-12-06 17:45:45 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,37 @@
+let score = "300abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"300" => 300
+//"300abc" => NaN
+//true => 1
+//false => 0
+//null => 0
+//undefined => NaN
+
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//1 => true
+//0 => false
+//"Aakash" => true
+//"" => false
+//null => false
+//undefined => false
+//NaN => false  
+
+
+let someNumber = 123;
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
