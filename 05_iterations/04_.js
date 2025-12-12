a7b17dd6b36c1c56f01e12cbbb6f7bb6dbde08c8
Aakash <aakashkumarx13@gmail.com>
2025-12-12 19:40:20 +0000

    Loops and DOM start

---
+++ 05_iterations/04_.js
@@ -0,0 +1,31 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+// for (const key in myObject) {
+//     console.log(key);
+// }
+for (const key in myObject) {
+    // console.log(myObject[key]);
+    //  node ./05_iterations/04_.jsconsole.log(`${key} shortcut is for ${myObject[key]} `);
+    
+}
+
+// const programming = ["js", "cpp", "c", "rb"]
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+    
+// }
+
+// const map = new Map()
+// map.set('IN', 'India')
+// map.set('USA', 'United States of America')
+// map.set('FR', 'France')
+
+// for (const key in map) {
+//     console.log(key);
+// }
